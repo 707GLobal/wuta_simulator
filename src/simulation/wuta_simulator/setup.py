021631d829bd36ad4dc75e4a32a9f32abc9e7318
707GLobal <2578034340@qq.com>
2026-07-09 15:10:40 +0800

    小修改

--- src/simulation/wuta_simulator/setup.py
+++ src/simulation/wuta_simulator/setup.py
@@ -14,8 +14,8 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='czh',
-    maintainer_email='czh@todo.todo',
+    maintainer='wuta',
+    maintainer_email='wuta@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
     extras_require={
